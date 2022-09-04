f9674753bec9a31fd085df5fe35ee27d4287eece
Benjamin Pazienza <pazienza.log@gmail.com>
2022-09-04 07:56:44 -0400

    Updating get endpoint url

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -8,7 +8,7 @@ import { catchError, retry } from 'rxjs/operators';
 /*
  * The path to the endpoint returning the list of all blog entries. 
  */
-const GET_ALL_BLOGS_ENDPOINT = "http://71.192.160.144:10443/manage/all";
+const GET_ALL_BLOGS_ENDPOINT = "http://localhost:34000/manage/all";
 
 @Injectable({
   providedIn: 'root'
